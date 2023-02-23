ae7873043fdaa05b6583845b750cc767d5c22f05
Pavel <Test@mail.ru>
2023-02-23 10:33:53 +0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 13_676;
+        int bonusOfPrice = 20;
+
+        int miles = ticketPrice / bonusOfPrice;
+
+        System.out.println(miles);
+    }
+}
